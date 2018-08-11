170dca371267a601a7e8c18e8c73edbc91622737
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-08-11 11:57:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,10 +5,26 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+     student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
   end
 
+# expect{Student.new({:name => "Sophie DeBenedetto", :location => "Brooklyn, NY"})}.to_not raise_error
+#       expect(student.name).to eq("Alex Patriquin")
+#       expect(student.location).to eq("New York, NY")
+
   def self.create_from_collection(students_array)
+
+    # student = students_array.each_slice(1).to_a
+    # students_array[0]
+    # students_array.each do |person| 
+    #   person.each do |k, v| 
+        
+    #     "#{v}"
+    students_array.each do |s| 
+      
+binding.pry
+put :name
     
   end
 
@@ -17,7 +33,7 @@ class Student
   end
 
   def self.all
-    
+    @@all
   end
 end
 
